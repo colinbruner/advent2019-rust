f46cb2fb766b159eff253b34ad9fe7215e1bb25d
Colin Bruner <colin.d.bruner@gmail.com>
2020-04-22 20:29:54 -0400

    day2 part2 complete

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-fn calcu(operation: usize, cursor: usize, program: &mut Vec<usize>) {
+fn calc(operation: usize, cursor: usize, program: &mut Vec<usize>) {
     // Determine first, second and third values
     let op1 = program[program[cursor + 1]];
     let op2 = program[program[cursor + 2]];
@@ -15,30 +15,56 @@ fn calcu(operation: usize, cursor: usize, program: &mut Vec<usize>) {
     program[pos] = res
 }
 
+fn int_code_program(mut program: &mut Vec<usize>, mut cursor: usize) -> usize {
+    while program[cursor] != 99 {
+        if program[cursor] == 1 {
+            calc(1, cursor, &mut program)
+        } else if program[cursor] == 2 {
+            calc(2, cursor, &mut program)
+        }
+        cursor += 4;
+    }
+    let res = program[0];
+    res
+}
+
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
+    let lines = BufReader::new(file).lines().map(|l| l.unwrap());
 
     // ASSUMPTION: 1 line contains 1 "program" run.
-    for line in reader.lines() {
-        let mut cursor: usize = 0;
+    for line in lines {
+        let cursor: usize = 0;
         // Split line by ',' and parse to a vec<u32> ints
-        let mut program = line.unwrap()
+        let orig_program = line
             .split(",")
             .filter_map(|p| p.parse::<usize>().ok())
             .collect::<Vec<usize>>();
 
-        while program[cursor] != 99 {
-            if program[cursor] == 1 {
-                calcu(1, cursor, &mut program)
-            } else if program[cursor] == 2 {
-                calcu(2, cursor, &mut program)
+        let target: usize = 19690720;
+        let mut noun: usize = 0;
+
+        while noun != 99 {
+            for i in 0..99 {
+                let verb = i;
+                let mut program = Vec::new();
+                program.extend(&orig_program);
+
+                program[1] = noun;
+                program[2] = verb;
+
+                let res = int_code_program(&mut program, cursor);
+                if res == target {
+                    let ans_noun = noun;
+                    let ans_verb = verb;
+                    println!("Target {:?}", res);
+                    println!("Answer {:?}", 100 * ans_noun + ans_verb);
+
+                } else {
+                };
             }
-            cursor += 4;
+            noun += 1;
         }
-
-        println!("{:?}", program[0]);
-
     }
 
     Ok(())
