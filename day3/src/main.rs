ca976acba2e719dd755031b8f6a46fa05aff117c
Colin Bruner <colin.d.bruner@gmail.com>
2020-05-07 21:13:54 -0400

    day3 complete, with assistance

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,75 +1,109 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
-use std::collections::HashMap;
+// All credit for this code goes to https://dev.to/rpalo/comment/ig0e 
 
+use std::fs;
+use std::ops::Add;
+use std::iter::FromIterator;
+use std::collections::{HashMap, HashSet};
 
-enum Wire {
-    Move(String, i32),
-    Up(i32),
-    Down(i32),
-    Left(i32),
-    Right(i32),
-    Position { x: i32, y: i32 }
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct Coordinate {
+    x: isize,
+    y: isize,
+
 }
 
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-impl Wire {
-    fn Up(&self, dis: i32) {
-        &self::Position::y + dis
+impl Coordinate {
+    pub fn new(x: isize, y: isize) -> Self {
+        Self {x, y}
     }
 }
 
-fn format_path(direction: &str, pathes: &mut HashMap<String, u32>) {
-    // U,D,L,R
-    let path = direction.chars().next().unwrap().to_string();
+impl Add for Coordinate {
+    type Output = Self;
 
-    // Take all the string chars, except the zeroth element, and convert to u32.
-    let distance: u32 = direction.chars().as_str()[1..].parse::<u32>().unwrap();
-
-    pathes.insert(path, distance);
-}
-
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
-    let input = BufReader::new(file).lines().map(|l| l.unwrap());
-
-    // Contains as many elements are "wires" to check pathes for
-    let mut wires: Vec<Vec<HashMap<String, u32>>> = Vec::new();
-
-    for path in input {
-        // Contains HashMaps of all K: Path -> V: Distance
-        let mut all_pathes: Vec<HashMap<String, u32>> = Vec::new();
-
-        // Split all paths in input by ','
-        for direction in path.split(",") {
-
-            // K: Path will be U, D, L, R.
-            // V: Distance will be an integer containing manhatten distance hops
-            let mut pathes: HashMap<String, u32> = HashMap::new();
-
-            // Split input and gather data
-            format_path(direction, &mut pathes);
-
-            // Push to vec holding data
-            all_pathes.push(pathes);
-        }
-        // Represents the wire being runs pathes
-        wires.push(all_pathes);
-    }
-
-    for wire_path in wires {
-        for path in wire_path {
-            for (dir, dis) in path {
-                println!("{:?}", dir);
-                println!("{:?}", dis);
-            }
-            println!("---------------------------------");
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
         }
     }
+}
 
-    Ok(())
+type Wire = Vec<Coordinate>;
+type Moves = Vec<Coordinate>;
+
+fn format_instructions(wire_instructions: &str) -> Moves {
+
+    // Represents each x, y coordinate move-by-move
+    let mut moves: Moves = Vec::new();
+
+    // For each wire, parse instructions and 
+    for task in wire_instructions.split(",") {
+        // turn: U,D,L,R distance: int
+        let (turn, distance_text) = task.split_at(1);
+        let distance: usize = distance_text.parse().unwrap();
+
+        let coord = match turn {
+            "U" => Coordinate::new(0, 1),
+            "R" => Coordinate::new(1, 0),
+            "D" => Coordinate::new(0, -1),
+            "L" => Coordinate::new(-1, 0),
+            _   => panic!("Direction {:?} not valid.", turn)
+        };
+
+        for _ in 0..distance {
+            //println!("{:?}", c);
+            moves.push(coord);
+        }
+
+    }
+    moves
+}
+
+/// Build a Wire out of relative Moves
+fn make_wire(moves: Moves) -> Wire {
+    let mut current = Coordinate { x: 0, y: 0 };
+    let mut results: Wire = Vec::new();
+
+    for step in moves {
+        current = step + current;
+        results.push(current);
+
+    }
+
+    results
+}
+
+fn manhattan_distance(coord: &Coordinate) -> usize {
+    (coord.x.abs() + coord.y.abs()) as usize
+}
+
+fn find_closest_cross(wire_1: &Wire, wire_2: &Wire) -> Coordinate {
+    let wire_1: HashSet<&Coordinate> = HashSet::from_iter(wire_1.iter());
+    let wire_2: HashSet<&Coordinate> = HashSet::from_iter(wire_2.iter());
+    **wire_1.intersection(&wire_2).min_by_key(|c| manhattan_distance(c)).unwrap()
+}
+
+fn find_in_wire(wire: &Wire, target: &Coordinate) -> usize {
+    wire.iter().position(|e| e == target).unwrap() + 1
+}
+
+fn shortest_cross_distance(a: &Wire, b: &Wire) -> usize {
+    let a_set: HashSet<&Coordinate> = HashSet::from_iter(a.iter());
+    let b_set: HashSet<&Coordinate> = HashSet::from_iter(b.iter());
+    a_set.intersection(&b_set).map(|c| find_in_wire(a, c) + find_in_wire(b, c)).min().unwrap()
+}
+
+fn main() {
+    let text = fs::read_to_string("input.txt").unwrap();
+    let lines: Vec<&str> = text.split("\n").collect();
+
+    let mut wires = lines.into_iter().map(format_instructions).map(make_wire);
+
+    let (wire1, wire2) = (wires.next().unwrap(), wires.next().unwrap());
+
+    let closest_cross = find_closest_cross(&wire1, &wire2);
+    println!("Manhatten Distance of closest cross {:?}", manhattan_distance(&closest_cross));
+    println!("Fewest combined steps: {}", shortest_cross_distance(&wire1, &wire2));
+
 }
